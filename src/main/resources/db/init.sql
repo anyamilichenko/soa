f30de52fd890bf65ebba25ecd112978995110232
anyamilichenko <annaburmatova395@gmail.com>
2025-12-02 22:43:31 +0300

    теперь работает на 17 джаве!

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,4 +1,10 @@
--- Сначала создаем таблицу coordinates
+-- Временная таблица, которая будет генерироваться первой и "отвлекать" jOOQ
+CREATE TABLE IF NOT EXISTS __jooq_temp_fix (
+                                               id BIGSERIAL PRIMARY KEY,
+                                               dummy_field VARCHAR(10) DEFAULT 'dummy'
+);
+
+-- Создаем таблицу coordinates
 CREATE TABLE coordinates (
                              id BIGSERIAL PRIMARY KEY,
                              x BIGINT NOT NULL CHECK (x > -446),
@@ -27,3 +33,7 @@ CREATE TABLE dragon (
 -- Устанавливаем значения по умолчанию
 ALTER TABLE dragon
     ALTER COLUMN creation_date SET DEFAULT now();
+
+-- Вставляем dummy-запись в временную таблицу
+INSERT INTO __jooq_temp_fix (dummy_field) VALUES ('jooq-fix')
+ON CONFLICT DO NOTHING;
